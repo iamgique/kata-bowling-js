20169f63580c1d108c447c3692b4f6728abb894a
iamgique <iamgique@gmail.com>
2021-03-12 10:48:39 +0700

    add logic correct score when a spare is rolled

--- src/__tests__/kata-bowling-test.js
+++ src/__tests__/kata-bowling-test.js
@@ -1,11 +1,31 @@
 const KataBowling = require('../kata-bowling');
 
+let game;
+
+beforeEach(() => {
+    game = new KataBowling();
+})
+
 it ('should return 0 for a game of all zeros', () => {
-    const game = new KataBowling();
-
-    for (let i = 0; i < 20; i++) {
-        game.roll(0);
-    }
-
+    rollBall(0, 20);
     expect(game.score).toEqual(0);
 });
+
+it ('should return 20 for a game of all ones', () => {
+    rollBall(1, 20);
+    expect(game.score).toEqual(20);
+});
+
+it ('should return the correct score when a spare is rolled', () => {
+    game.roll(4);
+    game.roll(6);
+    game.roll(9);
+    rollBall(0, 17);
+    expect(game.score).toEqual(28)
+});
+
+function rollBall(pins, rolls) {
+    for (let i = 0; i < rolls; i++) {
+        game.roll(pins);
+    }
+}
---
+++ src/kata-bowling.js
@@ -0,0 +1,30 @@
+class KataBowling {
+
+    constructor() {
+        this.rolls = [];
+    }
+
+    roll(pins) {
+        this.rolls.push(pins);
+    }
+
+    get score() {
+        let score = 0;
+        let rollIndex = 0;
+
+        for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+            const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+            
+            if(frameScore === 10) {
+                score += 10 + this.rolls[rollIndex + 2];
+            } else {
+                score += frameScore;
+            }
+            rollIndex += 2;
+        }
+        return score;
+    }
+
+}
+
+module.exports = KataBowling;
