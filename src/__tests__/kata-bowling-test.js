1155c9efabfd4bdcb91144341f5524b40f4c0c7f
iamgique <iamgique@gmail.com>
2021-03-15 14:49:02 +0700

    add test and logic

--- src/__tests__/kata-bowling-test.js
+++ src/__tests__/kata-bowling-test.js
@@ -21,7 +21,71 @@ it ('should return the correct score when a spare is rolled', () => {
     game.roll(6);
     game.roll(9);
     rollBall(0, 17);
-    expect(game.score).toEqual(28)
+    expect(game.score).toEqual(28);
+});
+
+it ('should return the correct score when a strike is rolled', () => {
+    game.roll(10);
+    game.roll(1);
+    game.roll(2);
+    rollBall(0, 17);
+    expect(game.score).toEqual(16);
+});
+
+it ('should return the correct score when spare in frame 10', () => {
+    game.roll(6);
+    game.roll(4);
+    game.roll(10);
+    game.roll(8);
+    game.roll(1);
+    game.roll(10);
+    game.roll(7);
+    game.roll(0);
+    game.roll(10);
+    game.roll(7);
+    game.roll(2);
+    game.roll(10);
+    game.roll(10);
+    game.roll(7);
+    game.roll(3);
+    game.roll(1);
+    expect(game.score).toEqual(158);
+});
+
+it ('should return the correct score when strike in frame 10', () => {
+    game.roll(6);
+    game.roll(4);
+    game.roll(10);
+    game.roll(8);
+    game.roll(1);
+    game.roll(10);
+    game.roll(7);
+    game.roll(0);
+    game.roll(10);
+    game.roll(7);
+    game.roll(2);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(7);
+    game.roll(1);
+    expect(game.score).toEqual(175);
+});
+
+it ('should return the correct score when a strike 12 times', () => {
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    game.roll(10);
+    expect(game.score).toEqual(300);
 });
 
 function rollBall(pins, rolls) {
